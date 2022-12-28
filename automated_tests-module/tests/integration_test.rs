1970246fa6b7fa65de40471c5fed95d3bac980c9
Arda Yuvarlak <ardayuvarlak@gmail.com>
2022-12-27 18:42:10 -0800

    homeworks

--- automated_tests-module/tests/integration_test.rs
+++ automated_tests-module/tests/integration_test.rs
@@ -1,5 +1,5 @@
 //Eger package imiz binary ise calismayacaktir integration test, sadece lib crate icin var
-//Ayni zamanda binary crate te asagidaki gibi use kullanimi yapamayiz
+//Ayni zamanda binary crate te asagidaki gibi use kullanimi yapamayiz   
 use automated_tests_module;
 //Yukardaki gibi crate imizi scope a getirmemiz lazim cunku burdaki her test dosyasi kendisi crate gibi calisiyor
 mod common;
