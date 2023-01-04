84721875621aef00bb652feba9923ede5d0de2f3
Arda Yuvarlak <ardayuvarlak@gmail.com>
2023-01-03 23:27:18 -0800

    bug fix

--- publishing_crate/target/package/publishing-crate-0.1.0/target/debug/deps/publishing_crate-74de8a71572b0126.d
+++ publishing_crate/target/package/publishing-crate-0.1.0/target/debug/deps/publishing_crate-74de8a71572b0126.d
@@ -1,7 +1,7 @@
-C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\debug\deps\publishing_crate-74de8a71572b0126.rmeta: src\lib.rs
+C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\package\publishing-crate-0.1.0\target\debug\deps\publishing_crate-74de8a71572b0126.rmeta: src\lib.rs
 
-C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\debug\deps\libpublishing_crate-74de8a71572b0126.rlib: src\lib.rs
+C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\package\publishing-crate-0.1.0\target\debug\deps\libpublishing_crate-74de8a71572b0126.rlib: src\lib.rs
 
-C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\debug\deps\publishing_crate-74de8a71572b0126.d: src\lib.rs
+C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\package\publishing-crate-0.1.0\target\debug\deps\publishing_crate-74de8a71572b0126.d: src\lib.rs
 
 src\lib.rs:
--- publishing_crate/target/package/publishing-crate-0.1.0/target/debug/deps/publishing_crate.d
+++ publishing_crate/target/package/publishing-crate-0.1.0/target/debug/deps/publishing_crate.d
@@ -1,5 +1,5 @@
-C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\debug\deps\publishing_crate.exe: src\main.rs
+C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\package\publishing-crate-0.1.0\target\debug\deps\publishing_crate.exe: src\main.rs
 
-C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\debug\deps\publishing_crate.d: src\main.rs
+C:\Users\arday\OneDrive\Masaüstü\rust-lessons\publishing_crate\target\package\publishing-crate-0.1.0\target\debug\deps\publishing_crate.d: src\main.rs
 
 src\main.rs:
